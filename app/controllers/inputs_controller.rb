56dae0fbf053659325ba9ce8b7b978cbcf1384af
alijm0806 <alijm1104@gmail.com>
2022-09-12 12:00:44 -0500

    Add test

--- app/controllers/inputs_controller.rb
+++ app/controllers/inputs_controller.rb
@@ -1,5 +1,5 @@
 class InputsController < ApplicationController
   def index
-    render json: {message: "heelo from index"}
+    render json: { message: "heelo from index", message2: "hello from Ali" }
   end
 end
